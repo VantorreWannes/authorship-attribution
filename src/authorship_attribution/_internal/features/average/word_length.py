ab58bf2dcfe6cef16f2f98a94c95448d85d001b0
Wannes Vantorre <vantorrewannes@gmail.com>
2025-05-18 12:56:25 +0200

    feature: Implement required AverageWordLengthFeature. #6

---
+++ src/authorship_attribution/_internal/features/average/word_length.py
@@ -0,0 +1,15 @@
+from authorship_attribution._internal.features.base.values import ValuesFeature
+
+
+class AverageWordLengthFeature(ValuesFeature):
+    def __init__(self, word_count: int, summed_word_lengths: int) -> None:
+        super().__init__(word_count=word_count, summed_word_lengths=summed_word_lengths)
+
+    def word_count(self) -> int:
+        return getattr(self, "word_count")
+
+    def summed_word_lengths(self) -> int:
+        return getattr(self, "summed_word_lengths")
+
+    def average_word_length(self) -> float:
+        return self.summed_word_lengths() / self.word_count()
