6141bdf786885f13c3b5bf884e20490f0f12656a
Wannes Vantorre <vantorrewannes@gmail.com>
2025-05-18 13:05:11 +0200

    fix: Fix getter bug in AverageWordLengthFeature. #3

--- src/authorship_attribution/_internal/features/average/word_length.py
+++ src/authorship_attribution/_internal/features/average/word_length.py
@@ -3,13 +3,13 @@ from authorship_attribution._internal.features.base.values import ValuesFeature
 
 class AverageWordLengthFeature(ValuesFeature):
     def __init__(self, word_count: int, summed_word_lengths: int) -> None:
-        super().__init__(word_count=word_count, summed_word_lengths=summed_word_lengths)
+        super().__init__(__word_count=word_count, __summed_word_lengths=summed_word_lengths)
 
     def word_count(self) -> int:
-        return getattr(self, "word_count")
+        return getattr(self, "__word_count")
 
     def summed_word_lengths(self) -> int:
-        return getattr(self, "summed_word_lengths")
+        return getattr(self, "__summed_word_lengths")
 
     def average_word_length(self) -> float:
         return self.summed_word_lengths() / self.word_count()
