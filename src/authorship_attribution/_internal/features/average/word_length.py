f97e7a2b609984beb4ca21c24a0456c8aed2103b
Wannes Vantorre <vantorrewannes@gmail.com>
2025-05-18 16:13:43 +0200

    refactor: Refactor ValuesFeature to be more robust. #12

--- src/authorship_attribution/_internal/features/average/word_length.py
+++ src/authorship_attribution/_internal/features/average/word_length.py
@@ -3,13 +3,9 @@ from authorship_attribution._internal.features.base.values import ValuesFeature
 
 class AverageWordLengthFeature(ValuesFeature):
     def __init__(self, word_count: int, summed_word_lengths: int) -> None:
-        super().__init__(__word_count=word_count, __summed_word_lengths=summed_word_lengths)
-
-    def word_count(self) -> int:
-        return getattr(self, "__word_count")
-
-    def summed_word_lengths(self) -> int:
-        return getattr(self, "__summed_word_lengths")
+        super().__init__(word_count=word_count, summed_word_lengths=summed_word_lengths)
+        self.word_count = self._get("word_count")
+        self.summed_word_lengths = self._get("summed_word_lengths")
 
     def average_word_length(self) -> float:
-        return self.summed_word_lengths() / self.word_count()
+        return self.summed_word_lengths / self.word_count
