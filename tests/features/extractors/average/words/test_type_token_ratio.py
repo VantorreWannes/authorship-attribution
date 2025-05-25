d4fd733fca4ed340f406e8a76117ff122611bd94
Wannes Vantorre <vantorrewannes@gmail.com>
2025-05-25 13:49:33 +0200

    refactor: Improve TypeTokenRatioFeatureExtractor unit tests edge case handeling.

--- tests/features/extractors/average/words/test_type_token_ratio.py
+++ tests/features/extractors/average/words/test_type_token_ratio.py
@@ -1,44 +1,78 @@
-from typing import Literal
+from authorship_attribution._internal.features.average.words.type_token_ratio import (
+    TypeTokenRatioFeature,
+)
 
+
+from typing import Literal
+from pytest import fixture, approx
 
 from authorship_attribution._internal.features.extractors.average.words.type_token_ratio import (
     TypeTokenRatioFeatureExtractor,
 )
 from authorship_attribution._internal.types.aliases import Text
-from pytest import fixture
 
 
 @fixture
-def text() -> Text:
-    return "He he ha ha"
+def sample_text() -> Text:
+    return "He he ha ha. Test test."
 
 
 @fixture
-def type_token_ratio_feature_extractor(
-    text: Text,
-) -> TypeTokenRatioFeatureExtractor:
-    return TypeTokenRatioFeatureExtractor(text)
+def empty_text() -> Text:
+    return ""
 
 
 @fixture
-def unique_words_count() -> Literal[2]:
-    return 2
+def extractor(sample_text: Text) -> TypeTokenRatioFeatureExtractor:
+    return TypeTokenRatioFeatureExtractor(text=sample_text)
 
 
 @fixture
-def all_words_count() -> Literal[4]:
+def empty_extractor(empty_text: Text) -> TypeTokenRatioFeatureExtractor:
+    return TypeTokenRatioFeatureExtractor(text=empty_text)
+
+
+@fixture
+def expected_unique_words_count() -> Literal[4]:
     return 4
 
 
+@fixture
+def expected_all_words_count() -> Literal[8]:
+    return 8
+
+
 def test_unique_words_count(
-    type_token_ratio_feature_extractor: TypeTokenRatioFeatureExtractor,
-    unique_words_count: int,
+    extractor: TypeTokenRatioFeatureExtractor,
+    expected_unique_words_count: int,
 ) -> None:
-    assert type_token_ratio_feature_extractor.unique_words_count() == unique_words_count
+    assert extractor.unique_words_count() == expected_unique_words_count
 
 
 def test_all_words_count(
-    type_token_ratio_feature_extractor: TypeTokenRatioFeatureExtractor,
-    all_words_count: int,
+    extractor: TypeTokenRatioFeatureExtractor,
+    expected_all_words_count: int,
 ) -> None:
-    assert type_token_ratio_feature_extractor.all_words_count() == all_words_count
+    assert extractor.all_words_count() == expected_all_words_count
+
+
+def test_feature_extraction(
+    extractor: TypeTokenRatioFeatureExtractor,
+    expected_unique_words_count: int,
+    expected_all_words_count: int,
+) -> None:
+    feature: TypeTokenRatioFeature = extractor.feature()
+    assert feature.unique_words_count == expected_unique_words_count
+    assert feature.all_words_count == expected_all_words_count
+    assert feature.type_token_ratio() == approx(
+        expected=expected_unique_words_count / expected_all_words_count
+    )
+
+
+def test_empty_text_extraction(empty_extractor: TypeTokenRatioFeatureExtractor) -> None:
+    assert empty_extractor.unique_words_count() == 0
+    assert empty_extractor.all_words_count() == 0
+    feature: TypeTokenRatioFeature = empty_extractor.feature()
+    assert feature.unique_words_count == 0
+    assert feature.all_words_count == 0
+    assert feature.type_token_ratio() == approx(0.0)
