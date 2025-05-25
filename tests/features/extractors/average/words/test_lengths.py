6cee133d4a20ecc0f69965b4f002e12dbf6163f7
Wannes Vantorre <vantorrewannes@gmail.com>
2025-05-25 15:12:40 +0200

    fix: Remove redundant imports. #19

--- tests/features/extractors/average/words/test_lengths.py
+++ tests/features/extractors/average/words/test_lengths.py
@@ -1,7 +1,4 @@
-from authorship_attribution._internal.features.average.words.lengths import AverageWordLengthFeature
-
-
-from pytest import fixture, approx
+from pytest import fixture
 
 from authorship_attribution._internal.features.extractors.average.words.lengths import (
     AverageWordLengthFeatureExtractor,
@@ -13,27 +10,14 @@ from authorship_attribution._internal.types.aliases import Text
 def sample_text() -> Text:
     return "This is a test text."
 
-
-@fixture
-def empty_text() -> Text:
-    return ""
-
-
 @fixture
 def extractor(sample_text: Text) -> AverageWordLengthFeatureExtractor:
     return AverageWordLengthFeatureExtractor(sample_text)
-
-
-@fixture
-def empty_extractor(empty_text: Text) -> AverageWordLengthFeatureExtractor:
-    return AverageWordLengthFeatureExtractor(empty_text)
-
 
 def test_word_count(
     extractor: AverageWordLengthFeatureExtractor,
 ) -> None:
     assert extractor.word_count() == 6
-
 
 def test_summed_word_lengths(
     extractor: AverageWordLengthFeatureExtractor,
