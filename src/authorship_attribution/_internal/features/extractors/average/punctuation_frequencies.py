15745005f878fa0974eaca15f454f375378007e0
Wannes Vantorre <vantorrewannes@gmail.com>
2025-05-29 17:01:00 +0200

    refactor: Use fitting Counter type for punctuation counts. #25

--- src/authorship_attribution/_internal/features/extractors/average/punctuation_frequencies.py
+++ src/authorship_attribution/_internal/features/extractors/average/punctuation_frequencies.py
@@ -1,4 +1,5 @@
 import string
+from typing import Counter
 from authorship_attribution._internal.features.average.punctuation_frequencies import (
     PunctuationFrequenciesFeature,
 )
@@ -11,19 +12,16 @@ from authorship_attribution._internal.types.aliases import Text
 class PunctuationFrequenciesFeatureExtractor(LexicalFeatureExtractor):
     def __init__(self, text: Text) -> None:
         super().__init__(text)
+        self.punctuations = [char for char in self.text if char in string.punctuation]
 
-    def punctuation_counts(self) -> dict[str, int]:
-        return {
-            char: self.text.count(char)
-            for char in set[str](self.text)
-            if char in string.punctuation
-        }
+    def punctuation_counts(self) -> Counter[str]:
+        return Counter[str](self.punctuations)
 
     def total_punctuation_count(self) -> int:
-        return len([char for char in self.text if char in string.punctuation])
+        return len(self.punctuations)
 
     def feature(self) -> PunctuationFrequenciesFeature:
-        punctuation_counts: dict[str, int] = self.punctuation_counts()
+        punctuation_counts: Counter[str] = self.punctuation_counts()
         total_punctuation_count: int = self.total_punctuation_count()
         return PunctuationFrequenciesFeature(
             punctuation_counts, total_punctuation_count
