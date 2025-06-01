e4950b7cd501cce94612d54b0a226c5b2ba87f88
Wannes Vantorre <vantorrewannes@gmail.com>
2025-06-01 14:44:31 +0200

    feature: Implement required PartOfSpeechTagFrequenciesFeatureExtractor. #28

---
+++ src/authorship_attribution/_internal/features/extractors/average/pos_tag_frequencies.py
@@ -0,0 +1,24 @@
+from collections import Counter
+from authorship_attribution._internal.features.average.pos_tag_frequencies import PartOfSpeechTagFrequenciesFeature
+from authorship_attribution._internal.features.extractors.base.pos_tags import (
+    PartOfSpeechTagFeatureExtractor,
+)
+from authorship_attribution._internal.types.aliases import Text
+
+
+class PartOfSpeechTagFrequenciesFeatureExtractor(PartOfSpeechTagFeatureExtractor):
+    def __init__(self, text: Text) -> None:
+        super().__init__(text)
+
+    def part_of_speach_tags_counts(self) -> Counter[str]:
+        return Counter[str](self.part_of_speech_tags)
+
+    def total_part_of_speach_tags_count(self) -> int:
+        return len(self.part_of_speech_tags)
+
+    def feature(self) -> PartOfSpeechTagFrequenciesFeature:
+        part_of_speach_tags_counts: Counter[str] = self.part_of_speach_tags_counts()
+        total_part_of_speach_tags_count: int = self.total_part_of_speach_tags_count()
+        return PartOfSpeechTagFrequenciesFeature(
+            part_of_speach_tags_counts, total_part_of_speach_tags_count
+        )
