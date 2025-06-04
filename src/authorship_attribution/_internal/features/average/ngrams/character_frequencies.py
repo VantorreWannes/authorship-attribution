684ff40ec6435445fca110ae9119d1aad1bec93b
Wannes Vantorre <vantorrewannes@gmail.com>
2025-06-04 15:20:45 +0200

    refactor: remove duplicate CharacterNgramFrequenciesFeature code. #30

--- src/authorship_attribution/_internal/features/average/ngrams/character_frequencies.py
+++ src/authorship_attribution/_internal/features/average/ngrams/character_frequencies.py
@@ -1,19 +1,9 @@
 from collections import Counter
-from authorship_attribution._internal.features.base.base import Feature
+from authorship_attribution._internal.features.average.ngrams.frequencies import NgramFrequenciesFeature
 
 
-class CharacterNgramFrequenciesFeature(Feature):
-    pass
-
+class CharacterNgramFrequenciesFeature(NgramFrequenciesFeature):
     def __init__(self, ngram_counts: Counter[str], all_ngrams_count: int) -> None:
-        super().__init__()
-        self.ngram_counts: Counter[str] = ngram_counts
-        self.all_ngrams_count: int = all_ngrams_count
+        super().__init__(ngram_counts, all_ngrams_count)
 
-    def ngram_frequencies(self) -> dict[str, float]:
-        if self.all_ngrams_count == 0:
-            return dict[str, float]()
-        return {
-            char: count / self.all_ngrams_count
-            for char, count in self.ngram_counts.items()
-        }
+    
