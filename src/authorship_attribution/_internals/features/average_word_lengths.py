2c61438403207bdfa1ca1abeecd2564c2ab21de3
Wannes Vantorre <vantorrewannes@gmail.com>
2025-06-05 15:57:04 +0200

    feature: Implement AverageWordLengthFeatureExtractor type. #32

--- src/authorship_attribution/_internals/features/average_word_lengths.py
+++ src/authorship_attribution/_internals/features/average_word_lengths.py
@@ -1,4 +1,7 @@
-from authorship_attribution._internals.features.base import Feature
+from authorship_attribution._internals.features.base import (
+    Feature,
+    WordsFeatureExtractor,
+)
 
 
 class AverageWordLengthFeature(Feature):
@@ -11,3 +14,16 @@ class AverageWordLengthFeature(Feature):
         if self.words_count == 0:
             return 0.0
         return self.summed_word_lengths / self.words_count
+
+
+class AverageWordLengthFeatureExtractor(WordsFeatureExtractor):
+    def summed_word_lengths(self) -> int:
+        return sum(len(word) for word in self.words)
+
+    def words_count(self) -> int:
+        return len(self.words)
+
+    def feature(self) -> AverageWordLengthFeature:
+        summed_word_lengths: int = self.summed_word_lengths()
+        words_count: int = self.words_count()
+        return AverageWordLengthFeature(summed_word_lengths, words_count)
