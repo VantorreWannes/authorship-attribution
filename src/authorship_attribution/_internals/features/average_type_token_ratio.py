392e8b8c5d4a24bda0d7e041cdb8f01af489035c
Wannes Vantorre <vantorrewannes@gmail.com>
2025-06-06 14:19:41 +0200

    feature: Implement required AverageTypeTokenRatioFeatureExtractor type. #32

--- src/authorship_attribution/_internals/features/average_type_token_ratio.py
+++ src/authorship_attribution/_internals/features/average_type_token_ratio.py
@@ -1,6 +1,8 @@
 from authorship_attribution._internals.features.base import (
     Feature,
+    WordsFeatureExtractor,
 )
+from authorship_attribution._internals.types.aliases import Word
 
 
 class AverageTypeTokenRatioFeature(Feature):
@@ -13,3 +15,16 @@ class AverageTypeTokenRatioFeature(Feature):
         if self.words_count == 0:
             return 0.0
         return self.unique_words_count / self.words_count
+
+
+class AverageTypeTokenRatioFeatureExtractor(WordsFeatureExtractor):
+    def unique_words_count(self) -> int:
+        return len(set[Word](self.words))
+
+    def words_count(self) -> int:
+        return len(self.words)
+
+    def feature(self) -> AverageTypeTokenRatioFeature:
+        unique_words_count: int = self.unique_words_count()
+        words_count: int = self.words_count()
+        return AverageTypeTokenRatioFeature(unique_words_count, words_count)
