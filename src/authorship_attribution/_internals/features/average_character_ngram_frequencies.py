a65b88c1943c142f77d54940172535e7e5cfa033
Wannes Vantorre <vantorrewannes@gmail.com>
2025-06-21 19:34:57 +0200

    feature: Implement required AverageCharacterNgramFrequenciesFeatureExtractor type. #32

--- src/authorship_attribution/_internals/features/average_character_ngram_frequencies.py
+++ src/authorship_attribution/_internals/features/average_character_ngram_frequencies.py
@@ -1,6 +1,10 @@
 from collections import Counter
-from authorship_attribution._internals.features.base import Feature
-from authorship_attribution._internals.types.aliases import NGram
+from typing import override
+from authorship_attribution._internals.features.base import (
+    CharacterNgramsFeatureExtractor,
+    Feature,
+)
+from authorship_attribution._internals.types.aliases import NGram, Text
 
 
 class AverageCharacterNgramFrequenciesFeature(Feature):
@@ -18,3 +22,22 @@ class AverageCharacterNgramFrequenciesFeature(Feature):
             char: count / self.all_character_ngrams_count
             for char, count in self.character_ngram_counts.items()
         }
+
+
+class AverageCharacterNgramFrequenciesFeatureExtractor(CharacterNgramsFeatureExtractor):
+    def __init__(self, text: Text, ngram_size: int) -> None:
+        super().__init__(text, ngram_size)
+
+    def character_ngram_counts(self) -> Counter[NGram]:
+        return Counter[NGram](self.ngrams)
+
+    def all_character_ngrams_count(self) -> int:
+        return len(self.ngrams)
+
+    @override
+    def feature(self) -> AverageCharacterNgramFrequenciesFeature:
+        character_ngram_counts: Counter[NGram] = self.character_ngram_counts()
+        all_character_ngrams_count: int = self.all_character_ngrams_count()
+        return AverageCharacterNgramFrequenciesFeature(
+            character_ngram_counts, all_character_ngrams_count
+        )
